4d026b82a3edac4e9f3884f20e8b48d45ed02d56
OliverHarris <git@the-juggernaut.net>
2019-08-24 22:20:39 +0100

    Setup sidebar

--- react/my-app/src/ro.js
+++ react/my-app/src/ro.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {LoginForm} from './login.js'
+import {Dashboard} from './dash.js'
 function BasicExample() {
   CheckAuth()
   return (
     <Router>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
     
     </Router>
   );
---
+++ react/my-app/src/side.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class Side extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+        };
+
+    }
+    render() {
+
+        return (
+                <div>
+                    <a href="/dash">Dash</a>
+                    </div>
+                        );
+    }
+}
+
+
+export default Side
