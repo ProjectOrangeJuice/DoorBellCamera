0fc5747996aab0c7933d5900f2e43a816cf60fb7
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:47 +0100

    Api now sets to redis

--- api/configEdit.go
+++ api/configEdit.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/streadway/amqp"
 )
 
 type outMessage struct {
@@ -30,95 +28,18 @@ func getConfig(w http.ResponseWriter, r *http.Request) {
 
 //POST for setconfig
 func setConfig(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "plain/text")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	log.Print("On set")
 	params := mux.Vars(r)
 	body, err := ioutil.ReadAll(r.Body)
 	failOnError(err, "failed to read body")
-	setCommand(params["service"], string(body))
-}
-
-//Listens for the return of the config file
-func goListen(rch chan string, arg string) {
-	msgs, ch := listenToExchange("config", "config.test")
-	defer ch.Close()
-	rch <- "ready"
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-
-			rch <- decodeMsg(d.Body, arg)
-		}
-	}()
-
-	<-forever
-	fmt.Print("over 2")
-}
-
-func decodeMsg(msg []byte, arg string) string {
-	arg = strings.Replace(arg, ".", "-", -1)
-	var m outMessage
-	err := json.Unmarshal(msg, &m)
-	failOnError(err, "Json decode error")
-	return m.Inner
-}
-
-//Get the config file
-func getCommand(arg string) string {
-	returnCh := make(chan string)
-	go goListen(returnCh, arg)
-	if m := <-returnCh; m != "ready" {
-		log.Panicf("Something went wrong when waiting for ready")
+	var objmap map[string]*json.RawMessage
+	err = json.Unmarshal(body, &objmap)
+	failOnError(err, "Json error")
+	for index, key := range objmap {
+		var new string
+		new = strings.Replace(string(*key), string('"'), "", -1)
+		client.HSet(params["service"], index, new)
 	}
 
-	_, ch := listenToExchange("config", arg)
-	body := outMessage{"read", "test"}
-	b, err := json.Marshal(body)
-	failOnError(err, "Failed to make json")
-	defer ch.Close()
-	go func() {
-		err := ch.Publish(
-			"config", // exchange
-			arg,      // routing key
-			false,    // mandatory
-			false,    // immediate
-			amqp.Publishing{
-				ContentType: "application/json",
-				Body:        []byte(b),
-			})
-
-		failOnError(err, "Failed to publish a message")
-	}()
-
-	if m := <-returnCh; m == "error" {
-		log.Print("Something went wrong when returning")
-	} else {
-		return m
-	}
-	return "error"
-}
-
-//Set the config file
-func setCommand(arg string, config string) {
-
-	body := outMessage{"update", config}
-	b, err := json.Marshal(body)
-	_, ch := listenToExchange("config", arg)
-	defer ch.Close()
-
-	err = ch.Publish(
-		"config", // exchange
-		arg,      // routing key
-		false,    // mandatory
-		false,    // immediate
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        []byte(b),
-		})
-
-	failOnError(err, "Failed to publish a message")
-	log.Print("Sent new config")
-
+	//setCommand(params["service"], string(body))
 }
