dde5165d798fd5436d7dfa1c7cb85bf35fd44688
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:44 +0100

    remove cpu tests

--- v2/cpu-tests/rtspv.py
+++
@@ -1,8 +0,0 @@
-import rtsp
-
-with rtsp.Client(rtsp_server_uri = 'rtsp://192.168.1.120') as client:
-    _image = client.read()
-
-    while True:
-        #process_image(_image)
-        _image = client.read().show()
