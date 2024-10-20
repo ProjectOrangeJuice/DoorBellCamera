1c6b83213e3f404ddd623a35c38961b8d5d41b58
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:44 +0100

    get motion list

--- apiService/motion.go
+++ apiService/motion.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -32,5 +34,26 @@ func getMotions(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(records)
+
+}
+
+const videoLoc = "../storage/videos"
+
+func getMotion(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	log.Printf("Get video for %s, requested %s", r.RemoteAddr, params["code"])
+	http.ServeFile(w, r, fmt.Sprintf("%s/%s.mp4", videoLoc, params["code"]))
+
+}
+
+func deleteMotion(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	log.Printf("delete video for %s, requested %s", r.RemoteAddr, params["code"])
+
+	collection := conn.Collection("video")
+	filter := bson.M{"code": params["code"]}
+	collection.DeleteOne(context.TODO(), filter)
 
 }
