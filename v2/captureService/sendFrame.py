aa3530c3a4045da852dc4d459dfb7d50fb8c8517
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:49 +0100

    Send frames

--- v2/captureService/sendFrame.py
+++ v2/captureService/sendFrame.py
@@ -1,2 +1,9 @@
-def sendFrame(image,channel):
-    
+import datetime
+import json
+
+def sendFrame(image,name,channel):
+    #The json to send to rabbit
+    bodyText = {"cameraName":name,"time":str(datetime.datetime.now()),"image":image.decode('utf-8')}
+    channel.basic_publish(exchange='videoStream',
+        routing_key= name.replace(" ","."),
+        body= json.dumps(bodyText))
