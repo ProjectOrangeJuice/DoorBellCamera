f3d16cebe720ff95f004af554e2c84b1777d57a9
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:50 +0100

    delete unused code

--- storage/video_work.go
+++ storage/video_work.go
@@ -3,15 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
 
-	"github.com/icza/mjpeg"
 	"go.mongodb.org/mongo-driver/bson"
-	"gocv.io/x/gocv"
 )
 
 type cameraSettings struct {
@@ -84,137 +81,6 @@ func makeVideo(code string, name string) {
 		failOnError(err, "Failed to remove image")
 	}
 	addToDatabase(code, name, startTime, endTime, reasons)
-}
-
-func makeVideo3(code string, name string) {
-	log.Println("Make video")
-	collection := conn.Collection("settings")
-	filter := bson.M{"_id": 0}
-	doc := collection.FindOne(context.TODO(), filter)
-	var settings cameraSettings
-	doc.Decode(&settings)
-	aw, err := mjpeg.New(fmt.Sprintf("%s/%s.mp4", videoFolder, m.Code), 1280, 720, int32(settings.FPS)-5)
-	failOnError(err, "failed to setup video")
-
-	collection = conn.Collection("capture")
-	filter = bson.M{"code": code}
-	cur, _ := collection.Find(context.TODO(), filter)
-
-	var fr []string
-	var counter = 0
-	var reasons string
-	var startTime string
-	var endTime string
-	for cur.Next(context.TODO()) {
-		counter++
-		var record dbRecord
-		err := cur.Decode(&record)
-		failOnError(err, "decoding record")
-		if reasons == "" {
-			reasons = record.Reason
-		}
-		sp := strings.Split(record.Reason, ",")
-		if len(sp) > 1 {
-			for _, v := range sp {
-				spr := strings.Split(reasons, ",")
-				found := false
-				for _, v2 := range spr {
-					if v2 == v {
-						found = true
-					}
-				}
-				if !found {
-					if reasons == "" {
-						reasons = v
-					} else {
-						reasons = reasons + "," + v
-					}
-				}
-			}
-		}
-		if startTime == "" {
-			startTime = record.Time
-		} else {
-			endTime = record.Time
-		}
-		data, err := ioutil.ReadFile(fmt.Sprintf("%s", record.Location))
-		failOnError(err, "Failed to read image")
-		failOnError(aw.AddFrame(data), "Failed to add frame")
-	}
-
-	aw.Close()
-	for _, elem := range fr {
-		err := os.Remove(elem)
-		failOnError(err, "Failed to remove image")
-	}
-	addToDatabase(code, name, startTime, endTime, reasons)
-
-}
-
-func makeVideo2(code string, name string) {
-	var startTime string
-	var endTime string
-	video, err := gocv.VideoWriterFile(fmt.Sprintf("%s/%s.mp4", videoFolder, code), "avc1", 5.0, 1280, 720, true)
-
-	//aw, err := mjpeg.New(fmt.Sprintf("%s/%s", videoFolder, m.Code), 1280, 720, 10)
-	failOnError(err, "Setting up video")
-
-	collection := conn.Collection("capture")
-	filter := bson.M{"code": code}
-	cur, _ := collection.Find(context.TODO(), filter)
-
-	var fr []string
-	var counter = 0
-	var reasons string
-	for cur.Next(context.TODO()) {
-		counter++
-		var record dbRecord
-		err := cur.Decode(&record)
-		failOnError(err, "decoding record")
-		if reasons == "" {
-			reasons = record.Reason
-		}
-		sp := strings.Split(record.Reason, ",")
-		if len(sp) > 1 {
-			for _, v := range sp {
-				spr := strings.Split(reasons, ",")
-				found := false
-				for _, v2 := range spr {
-					if v2 == v {
-						found = true
-					}
-				}
-				if !found {
-					if reasons == "" {
-						reasons = v
-					} else {
-						reasons = reasons + "," + v
-					}
-				}
-			}
-		}
-		if startTime == "" {
-			startTime = record.Time
-		} else {
-			endTime = record.Time
-		}
-		im := gocv.IMRead(fmt.Sprintf("%s", record.Location), gocv.IMReadAnyColor)
-		video.Write(im)
-		im.Close()
-
-	}
-
-	//err = aw.Close()
-	//failOnError(err, "Error closing")
-	video.Close()
-	for _, elem := range fr {
-		err := os.Remove(elem)
-		failOnError(err, "Failed to remove image")
-	}
-	//video = nil
-	//log.Printf("Start time %s and end time %s", startTime, endTime)
-	addToDatabase(code, name, startTime, endTime, reasons)
-	//squashVideo(code)
 }
 
 type videoRecord struct {
