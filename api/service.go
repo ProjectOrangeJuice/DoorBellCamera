92a9dab022d7b05d852590533ceb2bc88b64581f
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:47 +0100

    Cors error

--- api/service.go
+++ api/service.go
@@ -16,14 +16,14 @@ func main() {
 	//Everything with /s/.. requires you to login
 	sec := router.PathPrefix("/s").Subrouter()
 	sec.Use(auth)
-	sec.HandleFunc("/refresh", refresh).Methods("GET")
-	sec.HandleFunc("/motion", allMotion).Methods("GET")
+	sec.HandleFunc("/refresh", refresh).Methods("GET", "OPTIONS")
+	sec.HandleFunc("/motion", allMotion).Methods("GET", "OPTIONS")
 	sec.HandleFunc("/motion/{code}", getMotion).Methods("DELETE", "GET", "OPTIONS")
 	sec.HandleFunc("/stream/{code}", getVideo).Methods("GET", "OPTIONS")
 	sec.HandleFunc("/service/motion", getVideo).Methods("GET", "OPTIONS")
 	sec.HandleFunc("/service/door", getVideo).Methods("GET", "OPTIONS")
-	sec.HandleFunc("/config/{service}", setConfig).Methods("POST")
-	sec.HandleFunc("/config/{service}", getConfig).Methods("GET")
+	sec.HandleFunc("/config/{service}", setConfig).Methods("POST", "OPTIONS")
+	sec.HandleFunc("/config/{service}", getConfig).Methods("GET", "OPTIONS")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
