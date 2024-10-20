e5c5eac9f6f6f9339ba0a76504283e8d2d7859e0
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:38 +0100

    Uses config and FPS

--- capture/getCamera.py
+++ capture/getCamera.py
@@ -4,21 +4,35 @@ import pika
 import json
 import datetime
 
-streamLocation = "rtsp://admin:admin@192.168.1.116/11"
+
+
+def readConfig():
+    global streamLocation,cameraName,serverAddress,serverPort,delay
+    with open("config.json") as jf:
+        data = json.load(jf)
+        streamLocation = data["cameraAddress"]
+        cameraName = data["cameraName"]
+        serverAddress = data["serverAddress"]
+        serverPort = data["serverPort"]
+        delay = 1/int(data["FPS"])
+
 
 
 def openConnection():
     print("Making connection")
     global connection,channel
-    connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100',31693))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(serverAddress,serverPort))
     channel = connection.channel()
     channel.queue_declare(queue='videoStream')
     
+
+readConfig()
 
 vcap = cv2.VideoCapture(streamLocation)
 openConnection()
 while(1):
     while(vcap.isOpened()):
+        start = time.time()
         try:
             ret, frame = vcap.read()
         except:
@@ -29,14 +43,18 @@ while(1):
         b64 = base64.b64encode(image)
         print("size of b64: "+str((len(b64)/1024)/1024))
         
-        bodyText = {"time":str(datetime.datetime.now()),"image":b64.decode('utf-8')}
+        bodyText = {"cameraName":cameraName,"time":str(datetime.datetime.now()),"image":b64.decode('utf-8')}
         channel.basic_publish(exchange='',
                     routing_key='videoStream',
                     body=json.dumps(bodyText))
     
        
-        
-        #time.sleep(1)
+        end = time.time()
+        total = (end-start)
+
+        makeDelay = delay - total
+        if(makeDelay > 0):
+            time.sleep(makeDelay)
 
 
     vcap = cv2.VideoCapture(streamLocation)
