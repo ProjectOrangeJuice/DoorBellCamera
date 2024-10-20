0877ba39837539ab0ff22437ac6db38eb4ba4404
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:43 +0100

    Started door service

---
+++ door/main.go
@@ -0,0 +1,18 @@
+package main
+
+type config struct {
+	ServerAddress string
+	ServerPort    int
+	Camereas      []cameras
+}
+type cameras struct {
+	Name string
+}
+
+func readConfig() {
+
+}
+
+func main() {
+
+}
