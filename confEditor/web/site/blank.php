6d1d64fda21a68917d1664a10f6bd50bc5b297d4
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:45 +0100

    Splitting of page

---
+++ confEditor/web/site/blank.php
@@ -0,0 +1,31 @@
+<?php 
+$title = "Hello world";
+include("parts/header.php");
+?>
+
+<body class="w3-light-grey">
+<?php include("parts/sidebar.php");?>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px;margin-top:43px;">
+
+  <!-- Header -->
+  <header class="w3-container" style="padding-top:22px">
+    <h5><b><i class="fa fa-dashboard"></i> Information</b></h5>
+  </header>
+
+  <div class="w3-row-padding w3-margin-bottom">
+abc
+  </div>
+
+ 
+
+
+ <?php include("parts/footer.php") ?>
+
+  <!-- End page content -->
+</div>
+
+
+</body>
+</html>
