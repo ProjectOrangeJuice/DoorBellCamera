d56547ecca7e66e5bbf79f43b15d4d77a2ac5cee
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:43 +0100

    Sends out notification for images to be converted

--- motion/keep_motion.go
+++ motion/keep_motion.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/streadway/amqp"
@@ -26,6 +27,15 @@ type Message struct {
 	Code  string
 	Count int
 }
+
+type OutMessage struct {
+	Code string
+}
+
+var prev string
+var notified string = "nothing"
+var ignoreTimer bool = true
+var timer time.Timer
 
 func main() {
 
@@ -67,6 +77,7 @@ func readyAndListen() {
 	)
 	failOnError(err, "Failed to register a consumer")
 
+	createTimer()
 	forever := make(chan bool)
 
 	go func() {
@@ -77,6 +88,21 @@ func readyAndListen() {
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
+}
+
+func createTimer() {
+	timer := time.NewTimer(5 * time.Second)
+	go func() {
+		<-timer.C
+		log.Printf("Timer is over")
+		if prev != "" && prev != notified && !ignoreTimer {
+			notified = prev
+			notifyQueue(prev)
+			prev = ""
+		}
+		ignoreTimer = false
+		createTimer()
+	}()
 }
 
 func decodeMessage(d []byte) {
@@ -100,6 +126,51 @@ func recordDb(msg Message, loc string) {
 	failOnError(err, "Record could not insert")
 	tx.Commit()
 	log.Printf("Saved to db")
+	if prev != "" && prev != msg.Code {
+		log.Printf("End of prev code")
+		notifyQueue(prev)
+		ignoreTimer = true
+		prev = msg.Code
+	} else if prev == "" {
+		prev = msg.Code
+		ignoreTimer = true
+
+	}
+}
+
+func notifyQueue(code string) {
+	log.Printf("NOTIFY")
+	body := OutMessage{code}
+	b, err := json.Marshal(body)
+	conn, err := amqp.Dial("amqp://guest:guest@192.168.99.100:31693/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"imageToVideo", // name
+		false,          // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        b,
+		})
+	failOnError(err, "Failed to publish a message")
+
 }
 
 func storeImage(msg Message) {
