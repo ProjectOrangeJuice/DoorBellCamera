2590934c68072cdf12b14c25caecd4f5184a1763
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:43 +0100

    Repair rabbit connection problem

--- capture/getCamera.py
+++ capture/getCamera.py
@@ -53,60 +53,70 @@ prev = 0
 vcap = cv2.VideoCapture(streamLocation)
 openConnection()
 #Should stream forever
-try:
-    while(1):
-        if minute_passed(timed):
-            readConfig()
-        while(vcap.isOpened()):
+
+def doWork():
+    global vcap,prev
+    try:
+        while(1):
             if minute_passed(timed):
                 readConfig()
-            #For fps
-            time_elapsed = time.time() - prev
-            if(time_elapsed > 1./delay):
-                try:
-                    ret, frame = vcap.read()
-                except:
-                    #Error with frame, try again.
-                    print("Error with frame")
-                    continue
-                
-                if(int(rotation) > 0):
+            while(vcap.isOpened()):
+                if minute_passed(timed):
+                    readConfig()
+                #For fps
+                time_elapsed = time.time() - prev
+                if(time_elapsed > 1./delay):
                     try:
-                        frame = rotateImage(frame,int(rotation))
-                    except AttributeError:
-                        print("An image failed")
-                        break
-                bval = int(blur)
-                if(bval > 0):
-                    frame = cv2.blur(frame,(bval,bval))
-                #kernel = np.ones((2,2),np.float32)/25
-                #frame = cv2.filter2D(frame,-1,kernel)
-                image = cv2.imencode(".jpg",frame)[1]
-               
-                b64 = base64.b64encode(image)
-                #Testing of sizes
-                #print("size of b64: "+str((len(b64)/1024)/1024))
+                        ret, frame = vcap.read()
+                    except:
+                        #Error with frame, try again.
+                        print("Error with frame")
+                        continue
+                    
+                    if(int(rotation) > 0):
+                        try:
+                            frame = rotateImage(frame,int(rotation))
+                        except AttributeError:
+                            print("An image failed")
+                            break
+                    bval = int(blur)
+                    if(bval > 0):
+                        frame = cv2.blur(frame,(bval,bval))
+                    #kernel = np.ones((2,2),np.float32)/25
+                    #frame = cv2.filter2D(frame,-1,kernel)
+                    image = cv2.imencode(".jpg",frame)[1]
                 
-                #The json to send to rabbit
-                bodyText = {"cameraName":cameraName,"time":str(datetime.datetime.now()),"image":b64.decode('utf-8')}
-                #TOPIC rabbit, with the topic being the camera name
+                    b64 = base64.b64encode(image)
+                    #Testing of sizes
+                    #print("size of b64: "+str((len(b64)/1024)/1024))
+                    
+                    #The json to send to rabbit
+                    bodyText = {"cameraName":cameraName,"time":str(datetime.datetime.now()),"image":b64.decode('utf-8')}
+                    #TOPIC rabbit, with the topic being the camera name
+                    
+                    channel.basic_publish(exchange='videoStream',
+                                routing_key=cameraName.replace(" ","."),
+                                body=json.dumps(bodyText))
                 
-                channel.basic_publish(exchange='videoStream',
-                            routing_key=cameraName.replace(" ","."),
-                            body=json.dumps(bodyText))
-            
-            
-                prev = time.time()
-            else:
-                #Skip this frame
-                vcap.grab()
-            #else:
-             #   time.sleep((1./delay)-time_elapsed)
-        #Delay reconnection attempt
+                
+                    prev = time.time()
+                else:
+                    #Skip this frame
+                    vcap.grab()
+                #else:
+                #   time.sleep((1./delay)-time_elapsed)
+            #Delay reconnection attempt
+            time.sleep(5)
+            vcap = cv2.VideoCapture(streamLocation)
+    except KeyboardInterrupt:
+        print("Bye!")
+        vcap.release() 
+        connection.close()
+while(1):
+    try:
+        doWork() 
+
+    except pika.exceptions.ChannelClosedByBroker:
+        openConnection()
         time.sleep(5)
-        vcap = cv2.VideoCapture(streamLocation)
-    
-except KeyboardInterrupt:
-    print("Bye!")
-    vcap.release() 
-    connection.close()
+        print("Connection failed. So i'm trying to open it again")
