8a4b0aeb524826d477525fd9dc85e0ae172bdf1f
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:51 +0100

    Setup capture folder

--- captureService/sendFrame.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import json
-
-def sendFrame(image,name,channel):
-    #The json to send to rabbit
-    bodyText = {"cameraName":name,"time":str(datetime.datetime.now()),"image":image.decode('utf-8')}
-    channel.basic_publish(exchange='videoStream',
-        routing_key= name.replace(" ","."),
-        body= json.dumps(bodyText))
