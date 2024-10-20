7d8d16a7bb25f6946901a7fa457d20653e2dfbf8
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:42 +0100

    Example of how I can use the templates

--- web/service.go
+++ web/service.go
@@ -11,14 +11,30 @@ import (
 
 var connect amqp.Connection
 
+var templates *template.Template
+
 func main() {
+	//Initiate templates
+	var err error
+	templates, err = template.ParseFiles("web/index.html", "web/other.html")
+	failOnError(err, "Failed to read templates")
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/o", other).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8001", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("web/index.html", "web/other.html")
-	t.Execute(w, nil)
+	templates.ExecuteTemplate(w, "index", nil)
+}
+func other(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "other", nil)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
