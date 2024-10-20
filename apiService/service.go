2857c7cc30a129a9ef4e3d3b359f1019e2997118
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:51 +0100

    Search motion added to api

--- apiService/service.go
+++ apiService/service.go
@@ -40,6 +40,7 @@ func main() {
 	router.HandleFunc("/motionAlert", getMotionAlert).Methods("GET", "OPTIONS")
 	router.HandleFunc("/motion", getMotions).Methods("GET")
 	router.HandleFunc("/motion/{start}/{end}", getMotionBetweenDates).Methods("GET")
+	router.HandleFunc("/motion/{cam}/{start}/{end}", searchMotion).Methods("GET")
 	router.HandleFunc("/from24", getMotion24).Methods("GET")
 	router.HandleFunc("/motion/{code}", getMotion).Methods("GET")
 	router.HandleFunc("/motion/{code}", deleteMotion).Methods("DELETE")
