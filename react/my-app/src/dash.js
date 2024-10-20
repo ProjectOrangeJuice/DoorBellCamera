964e76197a04705896281320a87dfc843235099c
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:42 +0100

    Setup sidebar

---
+++ react/my-app/src/dash.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Side} from './side.js'
+export class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           
+        };
+      
+    
+    }
+
+    render() {
+
+        return (
+            <Side />
+
+        );
+    }
+}
+
+
+export default Dashboard
