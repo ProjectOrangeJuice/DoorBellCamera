aa0cb670d8397cd2c03b2d277df215b5b70bf8a1
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:39 +0100

    Streaming

--- confEditor/web/main.go
+++ confEditor/web/main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	"github.com/streadway/amqp"
 )
 
@@ -19,12 +20,89 @@ type OutMessage struct {
 	Inner string
 }
 
+type Message struct {
+	Image string
+	Time  string
+	Code  string
+	Count int
+	Name  string
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/config/{service}", getConfig).Methods("GET")
 	router.HandleFunc("/config/{service}", setConfig).Methods("POST")
-	//router.HandleFunc("/stream",wsHandler)
-	http.ListenAndServe(":8000", router)
+	router.HandleFunc("/stream", wsHandler)
+	go doStream()
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	failOnError(err, "Couldn't upgrade")
+	// register client
+	clients[ws] = true
+}
+
+func doStream() {
+	conn, err := amqp.Dial("amqp://guest:guest@192.168.99.100:31693/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"videoStream", // name
+		false,         // durable
+		false,         // delete when usused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			var m Message
+			err := json.Unmarshal(d.Body, &m)
+			failOnError(err, "Json decode error")
+
+			for client := range clients {
+				err := client.WriteMessage(websocket.TextMessage, []byte(m.Image))
+				if err != nil {
+					log.Printf("Websocket error: %s", err)
+					client.Close()
+					delete(clients, client)
+				}
+			}
+
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
 
 func getConfig(w http.ResponseWriter, r *http.Request) {
