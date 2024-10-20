8b071b88dfb9c958ca1a79d7fe65731572a1e93d
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:49:51 +0100

    timestamp search

--- apiService/service.go
+++ apiService/service.go
@@ -36,6 +36,7 @@ func main() {
 	router.HandleFunc("/mobilestream/{camera}", getCompressedVideo).Methods("GET", "OPTIONS")
 	router.HandleFunc("/motionAlert", getMotionAlert).Methods("GET", "OPTIONS")
 	router.HandleFunc("/motion", getMotions).Methods("GET")
+	router.HandleFunc("/motion/{start}/{end}", getMotionBetweenDates).Methods("GET")
 	router.HandleFunc("/from24", getMotion24).Methods("GET")
 	router.HandleFunc("/motion/{code}", getMotion).Methods("GET")
 	router.HandleFunc("/motion/{code}", deleteMotion).Methods("DELETE")
