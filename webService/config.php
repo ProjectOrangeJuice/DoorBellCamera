d25c0dbdd0773eb669994c887791c99a0766fd85
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:45 +0100

    Add settings to web page

--- webService/config.php
+++ webService/config.php
@@ -8,12 +8,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <style>
   body,
   h1,
@@ -43,17 +40,15 @@
 <body>
 
   <!-- Sidebar/menu -->
-  <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;"
-    id="mySidebar"><br>
-    <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft"
-      style="width:100%;font-size:22px">Close Menu</a>
+  <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+    <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
     <div class="w3-container">
       <h3 class="w3-padding-64"><b>House<br>Cam</b></h3>
     </div>
     <div class="w3-bar-block">
       <a href="/" onclick="w3_close()" class="w3-bar-item w3-button  w3-hover-white">Home</a>
       <a href="/live.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Live</a>
-      <a href="/com.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Mobile</a> 
+      <a href="/com.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Mobile</a>
       <a href="/motion.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Motion</a>
       <a href="/config.php" onclick="w3_close()" class="w3-bar-item w3-white w3-button w3-hover-white">Settings</a>
     </div>
@@ -66,8 +61,7 @@
   </header>
 
   <!-- Overlay effect when opening sidebar on small screens -->
-  <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu"
-    id="myOverlay"></div>
+  <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
   <!-- !PAGE CONTENT! -->
   <div class="w3-main" style="margin-left:340px;margin-right:40px">
@@ -94,6 +88,37 @@
         <label>FPS</label>
         <input class="form-control" v-model.number="fps">
       </div>
+
+      <div class="form-group">
+        <label>Blur Amount</label>
+        <input class="form-control" v-model.number="blur">
+      </div>
+      <div class="form-group">
+        <label>Box Jump (How far should a box jump before it's ignored)</label>
+        <input class="form-control" v-model.number="boxJump">
+      </div>
+      <div class="form-group">
+        <label>Debug (Add extra information to capture)</label>
+        <input class="form-control" type="checkbox" v-model="debug">
+      </div>
+      <div class="form-group">
+        <label>Buffer Before</label>
+        <input class="form-control" v-model.number="bufferBefore">
+      </div>
+      <div class="form-group">
+        <label>Buffer After</label>
+        <input class="form-control" v-model.number="bufferAfter">
+      </div>
+      <div class="form-group">
+        <label>Refresh background after </label>
+        <input class="form-control" v-model.number="refreshCount">
+      </div>
+
+      <div class="form-group">
+        <label>Small movement size</label>
+        <input class="form-control" v-model.number="smallMove">
+      </div>
+
       <h3>Zones</h3>
 
       <img v-bind:src="vid" width="500px" v-on:click="zone" id="zo"></img>
@@ -152,8 +177,7 @@
 
   <!-- W3.CSS Container -->
   <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px">
-    <p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank"
-        class="w3-hover-opacity">w3.css</a></p>
+    <p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></p>
   </div>
 
   <script>
@@ -175,10 +199,6 @@
       var captionText = document.getElementById("caption");
       captionText.innerHTML = element.alt;
     }
-
-
-
-
   </script>
 
 
@@ -195,26 +215,41 @@
         active: true,
         mincount: [],
         vid: "",
-        newZone: true
-      }, mounted() {
+        newZone: true,
+        smallMove: 20,
+        blur: 5,
+        boxJump: 20,
+        debug: true,
+        bufferBefore: 10,
+        bufferAfter: 10,
+        refreshCount: 5,
+      },
+      mounted() {
         axios
-          .get("http://<?php echo $_SERVER['HTTP_HOST'];?>:8000/config")
+          .get("http://<?php echo $_SERVER['HTTP_HOST']; ?>:8000/config")
           .then(response => {
             this.name = response.data.Name;
             this.connection = response.data.Connection;
             this.fps = response.data.FPS;
-            
-            if(response.data.Area != null){
+            this.blur = response.data.blur;
+            this.boxJump = response.data.boxJump;
+            this.debug = response.data.debug;
+            this.bufferBefore = response.data.bufferBefore;
+            this.bufferAfter = response.data.bufferAfter;
+            this.refreshCount = response.data.refreshCount;
+            this.smallMove = response.data.smallMove;
+
+            if (response.data.Area != null) {
               this.area = response.data.Area;
             }
-            if(response.data.Amount != null){
+            if (response.data.Amount != null) {
               this.amount = response.data.Amount;
             }
-            if(response.data.Threshold != null){
+            if (response.data.Threshold != null) {
               this.threshold = response.data.Threshold;
             }
-            if(response.data.MinCount != null){
-              this.mincount =response.data.MinCount;
+            if (response.data.MinCount != null) {
+              this.mincount = response.data.MinCount;
             }
             this.active = response.data.Motion;
             this.displayCamera();
@@ -225,27 +260,27 @@
       },
       methods: {
         displayCamera() {
-          var socket = new WebSocket("ws://<?php echo $_SERVER['HTTP_HOST'];?>:8000/mobilestream/" + encodeURI(this.name))
+          var socket = new WebSocket("ws://<?php echo $_SERVER['HTTP_HOST']; ?>:8000/mobilestream/" + encodeURI(this.name))
           // Log errors
-          socket.onclose = function (error) {
+          socket.onclose = function(error) {
             vid = "";
           };
           let self = this;
-          socket.onmessage = function (event) {
+          socket.onmessage = function(event) {
             decoded = atob(event.data)
             self.vid = "data:image/jpg;base64, " + event.data;
           }
         },
         zone(e) {
-          var  b = this.$el.getBoundingClientRect()
+          var b = this.$el.getBoundingClientRect()
           var i = document.getElementById("zo");
           var x = e.clientX - b.left;
           var y = e.clientY - b.top - i.height;
           //Get ratio, then find real x value.
-        
-          x = Math.round(x *(i.naturalWidth/i.width))
-          y = Math.round(y *(i.naturalHeight/i.height))
-          console.log("x "+x+ "y "+y)
+
+          x = Math.round(x * (i.naturalWidth / i.width))
+          y = Math.round(y * (i.naturalHeight / i.height))
+          console.log("x " + x + "y " + y)
           if (this.newZone) {
             this.amount.push(50);
             this.mincount.push(5);
@@ -265,9 +300,9 @@
           this.mincount.splice(index, 1);
           this.amount.splice(index, 1);
         },
-        save: function () {
+        save: function() {
           axios
-            .post("http://<?php echo $_SERVER['HTTP_HOST'];?>:8000/config", {
+            .post("http://<?php echo $_SERVER['HTTP_HOST']; ?>:8000/config", {
               Name: this.name,
               Connection: this.connection,
               FPS: this.fps,
@@ -276,6 +311,13 @@
               Threshold: this.threshold,
               MinCount: this.mincount,
               Motion: this.active,
+              Blur: this.blur,
+              BoxJump: this.boxJump,
+              Debug: this.debug,
+              BufferBefore: this.bufferBefore,
+              BufferAfter: this.bufferAfter,
+              RefreshCount: this.refreshCount,
+              SmallMove: this.smallMove,
             })
             .then(response => {
               alert("Saved");
