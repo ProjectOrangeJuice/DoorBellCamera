b9557d9fa14755a9c80ccb7715b4d999744dc199
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:45 +0100

    memory fix

--- storage/video_work.go
+++ storage/video_work.go
@@ -3,16 +3,93 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 
+	"github.com/icza/mjpeg"
 	"go.mongodb.org/mongo-driver/bson"
 	"gocv.io/x/gocv"
 )
 
+type cameraSettings struct {
+	Name       string
+	Connection string
+	FPS        int
+	Area       [][]int
+	Amount     []int
+	Threshold  []int
+	MinCount   []int
+	Motion     bool
+}
+
 func makeVideo(code string, name string) {
+	log.Println("Make video")
+	collection := conn.Collection("settings")
+	filter := bson.M{"_id": 0}
+	doc := collection.FindOne(context.TODO(), filter)
+	var settings cameraSettings
+	doc.Decode(&settings)
+	aw, err := mjpeg.New(fmt.Sprintf("%s/%s.mp4", videoFolder, m.Code), 1280, 720, int32(settings.FPS)-5)
+	failOnError(err, "failed to setup video")
+
+	collection = conn.Collection("capture")
+	filter = bson.M{"code": code}
+	cur, _ := collection.Find(context.TODO(), filter)
+
+	var fr []string
+	var counter = 0
+	var reasons string
+	var startTime string
+	var endTime string
+	for cur.Next(context.TODO()) {
+		counter++
+		var record dbRecord
+		err := cur.Decode(&record)
+		failOnError(err, "decoding record")
+		if reasons == "" {
+			reasons = record.Reason
+		}
+		sp := strings.Split(record.Reason, ",")
+		if len(sp) > 1 {
+			for _, v := range sp {
+				spr := strings.Split(reasons, ",")
+				found := false
+				for _, v2 := range spr {
+					if v2 == v {
+						found = true
+					}
+				}
+				if !found {
+					if reasons == "" {
+						reasons = v
+					} else {
+						reasons = reasons + "," + v
+					}
+				}
+			}
+		}
+		if startTime == "" {
+			startTime = record.Time
+		} else {
+			endTime = record.Time
+		}
+		data, err := ioutil.ReadFile(fmt.Sprintf("%s", record.Location))
+		failOnError(err, "Failed to read image")
+		failOnError(aw.AddFrame(data), "Failed to add frame")
+	}
+
+	aw.Close()
+	for _, elem := range fr {
+		err := os.Remove(elem)
+		failOnError(err, "Failed to remove image")
+	}
+	addToDatabase(code, name, startTime, endTime, reasons)
+
+}
+
+func makeVideo2(code string, name string) {
 	var startTime string
 	var endTime string
 	video, err := gocv.VideoWriterFile(fmt.Sprintf("%s/%s.mp4", videoFolder, code), "avc1", 5.0, 1280, 720, true)
@@ -69,11 +146,11 @@ func makeVideo(code string, name string) {
 	//failOnError(err, "Error closing")
 	video.Close()
 	for _, elem := range fr {
-		err = os.Remove(elem)
+		err := os.Remove(elem)
 		failOnError(err, "Failed to remove image")
 	}
-	video = nil
-	log.Printf("Start time %s and end time %s", startTime, endTime)
+	//video = nil
+	//log.Printf("Start time %s and end time %s", startTime, endTime)
 	addToDatabase(code, name, startTime, endTime, reasons)
 	//squashVideo(code)
 }
@@ -91,7 +168,7 @@ func addToDatabase(code string, name string, start string, end string, reason st
 	collection := conn.Collection("video")
 	collection.InsertOne(context.TODO(), r)
 
-	log.Printf("Saved to db")
+	//log.Printf("Saved to db")
 
 	//	_, err = db.Exec("DELETE FROM motion WHERE motionCode=?", code)
 	//	failOnError(err, "Couldn't delete motion records")
@@ -99,9 +176,9 @@ func addToDatabase(code string, name string, start string, end string, reason st
 }
 
 func squashVideo(code string) {
-	output, err := exec.Command("ffmpeg", "-i", fmt.Sprintf("%s/%s.mp4", videoFolder, code), "-crf", "50", fmt.Sprintf("%s/mobile/%s.mp4", videoFolder, code)).Output()
+	//output, err := exec.Command("ffmpeg", "-i", fmt.Sprintf("%s/%s.mp4", videoFolder, code), "-crf", "50", fmt.Sprintf("%s/mobile/%s.mp4", videoFolder, code)).Output()
 	//failOnError(err, "FAiled to compress video")
-	log.Printf("I failed to make compressed video. %s", err)
-	log.Printf("Output was %s", output)
-	log.Print("Finished making mobile")
+	//log.Printf("I failed to make compressed video. %s", err)
+	//log.Printf("Output was %s", output)
+	//log.Print("Finished making mobile")
 }
