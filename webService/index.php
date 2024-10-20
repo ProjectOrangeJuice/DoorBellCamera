6741c59558016a4e4c7d7fc11bef540cb496e54a
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:46 +0100

    new web interface pushed

--- webService/index.php
+++ webService/index.php
@@ -1,157 +1,151 @@
-<!DOCTYPE html>
-<html lang="en">
-<title>House cam</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-  crossorigin="anonymous"></script>
-<style>
-body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
-body {font-size:16px;}
-.w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
-.w3-half img:hover{opacity:1}
-</style>
+<?php
+$title = "Home";
+$current = 0;
+
+include "include/head.php";
+?>
+
 <body>
+    <?php include "include/side.php"; ?>
 
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
-  <div class="w3-container">
-    <h3 class="w3-padding-64"><b>House<br>Cam</b></h3>
-  </div>
-  <div class="w3-bar-block">
-    <a href="/" onclick="w3_close()" class="w3-bar-item w3-button w3-white w3-hover-white">Home</a> 
-    <a href="/live.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Live</a> 
-     
-    <a href="/motion.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Motion</a> 
-    <a href="/config.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Settings</a> 
-  </div>
-</nav>
 
-<!-- Top menu on small screens -->
-<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
-  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
-  <span>House Cam</span>
-</header>
+    <!-- !PAGE CONTENT! -->
+    <div class="w3-main" style="margin-left:340px;margin-right:40px">
 
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+        <!-- Header -->
+        <div class="w3-container" style="margin-top:80px" id="showcase">
+            <h1 class="w3-jumbo"><b>Home page</b></h1>
+            <hr style="width:50px;border:5px solid red" class="w3-round">
+        </div>
+        <div class="w3-row-padding" id="app">
+            <div v-for="camera in cameras">
+                <div class="w3-col" style="width:220px">
+                    <a v-bind:href="'/live.php?camera='+camera.Name"> <img width="200px" height="112px" v-bind:id="camera.Name+'CAMERA'"></a>
+                    <div v-bind:id="camera.Name+'ERROR'"></div>
+                    <button class="w3-button w3-green">Settings</button>
+                </div>
+                <div class="w3-rest">
+                    <table class="w3-table w3-third">
+                        <tr>
+                            <td>Name</td>
+                            <td>{{ camera.Name}} </td>
+                        </tr>
 
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:340px;margin-right:40px">
+                        <tr>
+                            <td>Last alert</td>
+                            <td> {{ dateChange(camera.LastAlert) }}</td>
+                        </tr>
 
-  <!-- Header -->
-  <div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>Summary</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Motion alerts from last 24 hours</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
-  </div>
-  
- 
-  <!-- Photo grid (modal) -->
-  <div class="w3-row-padding" id="app">
-      <div v-for="alert in alerts">
-        <img v-bind:src="alert.Thumbnail" v-on:click="showVideo(alert.Code)"/>
-        {{ alert.Code }} <br>
-        Occured at {{ dateChange(alert.Start) }} to {{ dateChange(alert.End) }}<br>
-        <button v-on:click="deleteCode(alert.Code)">Delete</button>
-        <hr>
-      </div>
-  <!-- The Modal -->
-<div id="id01" class="w3-modal">
-  <div class="w3-modal-content">
-    <div class="w3-container">
-      <span onclick="document.getElementById('id01').style.display='none'"
-      class="w3-button w3-display-topright">&times;</span>
-      <video controls :src="videoL" width="100%"></video>
-      <button onclick="document.getElementById('id01').style.display='none'">Close</button>
+                        <tr>
+                            <td>Alerts in 24 hours</td>
+                            <td><a :href="'/p2/motion.php?cam='+camera.Name"> {{ camera.Alerts24 }} </a></td>
+                        </tr>
+
+                    </table>
+
+
+                </div>
+
+            </div>
+        </div>
+        <!-- End page content -->
     </div>
-  </div>
-</div>
-<!-- End page content -->
-</div>
 
-<!-- W3.CSS Container -->
-<div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></p></div>
-
-<script>
-// Script to open and close sidebar
-function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("myOverlay").style.display = "none";
-}
-
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-</script>
+    <!-- W3.CSS Container -->
+    <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px">
+        <p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></p>
+    </div>
 
 
 
-<script>
-  var app = new Vue({
-    el: '#app',
-    data: {
-      videoL: "",
-    alerts: []
-    }, mounted() {
-     this.updateMotion();
-    
-    },
-    methods: {
-      dateChange(d){
-        date = new Date(d*1000);
-        return(date.toLocaleString());
-      },
-      deleteCode(code){
-        axios
-        .delete("http://<?php echo $_SERVER['HTTP_HOST'];?>:8000/motion/"+code)
-        .then(response => {
-          this.updateMotion()
- 
+
+    <script>
+        var app = new Vue({
+            el: '#app',
+            data: {
+                cameras: []
+            },
+            mounted() {
+                this.updateInfo();
+
+            },
+
+            methods: {
+                dateChange(d) {
+                    date = new Date(d * 1000);
+                    return (date.toLocaleString());
+                },
+                viewCam() {
+                    this.cameras.forEach(function(item) {
+                        console.log("Current item " + item.Name)
+                        loadVideo(item.Name);
+                    });
+                },
+                updateInfo() {
+                    axios
+                        .get("http://<?php echo $_SERVER['HTTP_HOST']; ?>:8000/information")
+                        .then(response => {
+                            this.cameras = response.data;
+
+                            setTimeout(function() {
+                                this.viewCam();
+                            }.bind(this), 100);
+                        })
+                        .catch(response => {
+                            console.log("Error " + response);
+                        });
+
+                }
+            }
         })
-        .catch(response => {
-          console.log("Error " + response);
-        });
-      },
-      updateMotion(){
-        axios
-        .get("http://<?php echo $_SERVER['HTTP_HOST'];?>:8000/from24")
-        .then(response => {
-          this.alerts = response.data;
- 
-        })
-        .catch(response => {
-          console.log("Error " + response);
-        });
-      },
-      showVideo(v){
-        console.log("v is .. "+v)
-        this.videoL = "http://<?php echo $_SERVER['HTTP_HOST'];?>:8000/motion/"+v;
-        console.log("video is "+this.videoL)
-        document.getElementById('id01').style.display='block'
-      }
-    }
-  })
-</script>
+    </script>
+
+
+    <script>
+        function loadVideo(name) {
+            var socket = "";
+            var imgErr = document.getElementById(name + "ERROR");
+            var imgBox = document.getElementById(name + "CAMERA");
+            var askClose = false;
+            //Close existing connection
+            try {
+                askClose = true;
+                socket.close();
+            } catch (err) {
+                askClose = false;
+                console.log("I tried to close the socket but got this " + err.message);
+            }
+
+
+            //Reset image err
+            imgErr.innerHTML = ""
+            //Set socket
+            socket = new WebSocket("ws://<?php echo $_SERVER['HTTP_HOST']; ?>:8000/mobilestream/" + encodeURI(name))
+
+            //Connect to socket
+            var update = function() {
+
+                // Log errors
+                socket.onclose = function(error) {
+                    if (!askClose) {
+                        imgErr.innerHTML = "Socket has been closed. Connection to camera has failed"
+                        imgBox.src = "";
+                    }
+                    askClose = false;
+                };
+
+                socket.onmessage = function(event) {
+                    if (event.data == "PING") {
+                        socket.send("PONG")
+                    } else {
+                        decoded = atob(event.data)
+                        imgBox.src = "data:image/jpg;base64, " + event.data
+                    }
+                }
+            };
+            window.setTimeout(update);
+        }
+    </script>
+
 
 </body>
-</html>
