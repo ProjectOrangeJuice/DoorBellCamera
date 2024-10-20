a2a6b08eeac2eb95ca660276e008470c705998cf
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:48:42 +0100

    Some logging

--- api/rabbit.go
+++ api/rabbit.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"log"
-
 	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		logger.Fatalf("%s: %s", msg, err)
 	}
 }
 
